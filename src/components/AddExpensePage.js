2acf0a35b5fc4c80b9d87c89fa4f9ab8dc0b5dd6
rsalazarhanson <rsalazarhanson@gmail.com>
2020-08-12 10:04:42 -0400

    First commit

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+
+const AddExpensePage = () => <div>This is my add expense component</div>;
+
+export default AddExpensePage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+
+const HelpPage = () => <div>This is my HELP page</div>;
+
+export default HelpPage;
